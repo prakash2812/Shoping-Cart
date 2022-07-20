a9905b970abe4ce9a5de39f6bc3db00c2024f0d1
prakash2812 <pltkval@gmail.com>
2022-07-20 15:26:55 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import CartState from '../context/GlobalContext';
 const Home = () => {
-  return <div>Home</div>;
+  console.log('data got', CartState());
+  /*  const {
+        state: { products },
+    } = contextData(); */
+  return (
+    <div>
+      {/* {products.map((product) => {
+                return (
+                    <div key={product.id}>
+                        <h1>{product.name}</h1>
+                    </div>
+                );
+            })} */}
+    </div>
+  );
 };
 
 export default Home;
